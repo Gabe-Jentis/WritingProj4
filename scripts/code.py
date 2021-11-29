c4e1f1dd77e56d11169a89390b831c985df816f5
Gabe-Jentis <70538294+Gabe-Jentis@users.noreply.github.com>
2021-11-28 21:57:10 -0500

    Update code.py

--- scripts/code.py
+++ scripts/code.py
@@ -1,25 +1,7 @@
-# Gabriel Jentis
-# EECE 5644
-# HW 2, Question 2
-
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-
-# Define True Vehicle Location
-ang = random.random() * 2 * np.pi
-truePos = [np.cos(ang), np.sin(ang)]  # Random Point on unit circle radius 1
-
-# Scale to be somewhere inside unit circle
-truePos[0] = truePos[0] * random.random()
-truePos[1] = truePos[1] * random.random()
-print("The true position of the vehicle is: " + str(truePos))
-
-# Generate Grid to make contours
-delta = 0.005
-x = np.arange(-2.0, 2.0, delta)
-y = np.arange(-2.0, 2.0, delta)
-X, Y = np.meshgrid(x, y)
+import argparse
 
 #  Generate K reference points
 def genRefPoints(k):
@@ -49,127 +31,154 @@ def MAPlikelihood(measurements, candPt, refs):
     likelihood += (0.3**2)/(K*(0.25**2)) * np.matmul(candPt, np.transpose(candPt))
     return likelihood
 
-# 1 ref point
-refs1 = genRefPoints(1)
-truePosRangeMes = [rangeMeasurement(truePos, r) for r in refs1]
-likelihoods1 = np.zeros([len(X), len(Y)])
-for i in range(len(X)):
-    for j in range(len(Y)):
-        likelihoods1[i, j] = MAPlikelihood(truePosRangeMes, [X[i][j], Y[i][j]], refs1)
+# # 1 ref point
+#
+#
+# # 2 ref points
+# refs2 = genRefPoints(2)
+# truePosRangeMes = [rangeMeasurement(truePos, r) for r in refs2]
+# likelihoods2 = np.zeros([len(X), len(Y)])
+# for i in range(len(X)):
+#     for j in range(len(Y)):
+#         likelihoods2[i, j] = MAPlikelihood(truePosRangeMes, [X[i][j], Y[i][j]], refs2)
+# print(MAPlikelihood(truePosRangeMes, truePos, refs2))
+#
+# fig, ax = plt.subplots()
+# CS = ax.contour(X, Y, likelihoods2, np.linspace(likelihoods2.min(), likelihoods2.max(), 30))
+# ax.plot(truePos[0], truePos[1], 'r+', label='True Position')
+# ax.plot([refs2[i][0] for i in range(len(refs2))], [refs2[i][1] for i in range(len(refs2))], 'bo', label='Reference point')
+# ax.clabel(CS, inline=True, fontsize=10)
+# ax.set_title('MAP Objective Function Contour Map: 2 Reference Points')
+# ax.set_xlabel('X')
+# ax.set_ylabel('Y')
+# plt.legend()
+# plt.show()
+#
+# # 3 ref points
+# refs3 = genRefPoints(3)
+# truePosRangeMes = [rangeMeasurement(truePos, r) for r in refs3]
+# likelihoods3 = np.zeros([len(X), len(Y)])
+# for i in range(len(X)):
+#     for j in range(len(Y)):
+#         likelihoods3[i, j] = MAPlikelihood(truePosRangeMes, [X[i][j], Y[i][j]], refs3)
+#
+# print(MAPlikelihood(truePosRangeMes, truePos, refs3))
+# fig, ax = plt.subplots()
+# CS = ax.contour(X, Y, likelihoods3, np.linspace(likelihoods3.min(), likelihoods3.max(), 30))
+# ax.plot(truePos[0], truePos[1], 'r+', label='True Position')
+# ax.plot([refs3[i][0] for i in range(len(refs3))], [refs3[i][1] for i in range(len(refs3))], 'bo', label='Reference point')
+# ax.clabel(CS, inline=True, fontsize=10)
+# ax.set_title('MAP Objective Function Contour Map: 3 Reference Points')
+# ax.set_xlabel('X')
+# ax.set_ylabel('Y')
+# plt.legend()
+# plt.show()
+#
+# # 4 ref points
+# refs4 = genRefPoints(4)
+# truePosRangeMes = [rangeMeasurement(truePos, r) for r in refs4]
+# likelihoods4 = np.zeros([len(X), len(Y)])
+# for i in range(len(X)):
+#     for j in range(len(Y)):
+#         likelihoods4[i, j] = MAPlikelihood(truePosRangeMes, [X[i][j], Y[i][j]], refs4)
+#
+# print(MAPlikelihood(truePosRangeMes, truePos, refs4))
+# fig, ax = plt.subplots()
+# CS = ax.contour(X, Y, likelihoods4, np.linspace(likelihoods4.min(), likelihoods4.max(), 30))
+# ax.plot(truePos[0], truePos[1], 'r+', label='True Position')
+# ax.plot([refs4[i][0] for i in range(len(refs4))], [refs4[i][1] for i in range(len(refs4))], 'bo', label='Reference point')
+# ax.clabel(CS, inline=True, fontsize=10)
+# ax.set_title('MAP Objective Function Contour Map: 4 Reference Points')
+# ax.set_xlabel('X')
+# ax.set_ylabel('Y')
+# plt.legend()
+# plt.show()
+#
+# # Plot Contours again, but using levels of 4 reference points
+# fig, ax = plt.subplots()
+# CS = ax.contour(X, Y, likelihoods1, np.linspace(likelihoods4.min(), likelihoods4.max(), 30))
+# ax.plot(truePos[0], truePos[1], 'r+', label='True Position')
+# ax.plot([refs1[i][0] for i in range(len(refs1))], [refs1[i][1] for i in range(len(refs1))], 'bo', label='Reference point')
+# ax.clabel(CS, inline=True, fontsize=10)
+# ax.set_title('MAP Objective Function Contour Map: 1 Reference Point')
+# ax.set_xlabel('X')
+# ax.set_ylabel('Y')
+# plt.legend()
+# plt.show()
+#
+# fig, ax = plt.subplots()
+# CS = ax.contour(X, Y, likelihoods2, np.linspace(likelihoods4.min(), likelihoods4.max(), 30))
+# ax.plot(truePos[0], truePos[1], 'r+', label='True Position')
+# ax.plot([refs2[i][0] for i in range(len(refs2))], [refs2[i][1] for i in range(len(refs2))], 'bo', label='Reference point')
+# ax.clabel(CS, inline=True, fontsize=10)
+# ax.set_title('MAP Objective Function Contour Map: 2 Reference Points')
+# ax.set_xlabel('X')
+# ax.set_ylabel('Y')
+# plt.legend()
+# plt.show()
+#
+# fig, ax = plt.subplots()
+# CS = ax.contour(X, Y, likelihoods3, np.linspace(likelihoods4.min(), likelihoods4.max(), 30))
+# ax.plot(truePos[0], truePos[1], 'r+', label='True Position')
+# ax.plot([refs3[i][0] for i in range(len(refs3))], [refs3[i][1] for i in range(len(refs3))], 'bo', label='Reference point')
+# ax.clabel(CS, inline=True, fontsize=10)
+# ax.set_title('MAP Objective Function Contour Map: 3 Reference Points')
+# ax.set_xlabel('X')
+# ax.set_ylabel('Y')
+# plt.legend()
+# plt.show()
+#
+# fig, ax = plt.subplots()
+# CS = ax.contour(X, Y, likelihoods4, np.linspace(likelihoods4.min(), likelihoods4.max(), 30))
+# ax.plot(truePos[0], truePos[1], 'r+', label='True Position')
+# ax.plot([refs4[i][0] for i in range(len(refs4))], [refs4[i][1] for i in range(len(refs4))], 'bo', label='Reference point')
+# ax.clabel(CS, inline=True, fontsize=10)
+# ax.set_title('MAP Objective Function Contour Map: 4 Reference Points')
+# ax.set_xlabel('X')
+# ax.set_ylabel('Y')
+# plt.legend()
+# plt.show()
 
-print(MAPlikelihood(truePosRangeMes, truePos, refs1))
-fig, ax = plt.subplots()
-CS = ax.contour(X, Y, likelihoods1, np.linspace(likelihoods1.min(), likelihoods1.max(), 30))
-ax.plot(truePos[0], truePos[1], 'r+', label='True Position')
-ax.plot([refs1[i][0] for i in range(len(refs1))], [refs1[i][1] for i in range(len(refs1))], 'bo', label='Reference point')
-ax.clabel(CS, inline=True, fontsize=10)
-ax.set_title('MAP Objective Function Contour Map: 1 Reference Point')
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-plt.legend()
-plt.show()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='This Program Creates a Contour Map to find the likelihood of the '
+                                                 + 'position of a vehicle located in the unit circle based on noisy '
+                                                 + 'measurements from a specified number of reference points')
+    parser.add_argument('-refs', '--r', dest='numRefs', type=int, required=True,
+                        help='Number of Reference Points')
 
-# 2 ref points
-refs2 = genRefPoints(2)
-truePosRangeMes = [rangeMeasurement(truePos, r) for r in refs2]
-likelihoods2 = np.zeros([len(X), len(Y)])
-for i in range(len(X)):
-    for j in range(len(Y)):
-        likelihoods2[i, j] = MAPlikelihood(truePosRangeMes, [X[i][j], Y[i][j]], refs2)
-print(MAPlikelihood(truePosRangeMes, truePos, refs2))
+    args = parser.parse_args()
+    print(args.numRefs)
+    # Define True Vehicle Location
+    ang = random.random() * 2 * np.pi
+    truePos = [np.cos(ang), np.sin(ang)]  # Random Point on unit circle radius 1
 
-fig, ax = plt.subplots()
-CS = ax.contour(X, Y, likelihoods2, np.linspace(likelihoods2.min(), likelihoods2.max(), 30))
-ax.plot(truePos[0], truePos[1], 'r+', label='True Position')
-ax.plot([refs2[i][0] for i in range(len(refs2))], [refs2[i][1] for i in range(len(refs2))], 'bo', label='Reference point')
-ax.clabel(CS, inline=True, fontsize=10)
-ax.set_title('MAP Objective Function Contour Map: 2 Reference Points')
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-plt.legend()
-plt.show()
+    # Scale to be somewhere inside unit circle
+    truePos[0] = truePos[0] * random.random()
+    truePos[1] = truePos[1] * random.random()
+    print("The true position of the vehicle is: " + str(truePos))
 
-# 3 ref points
-refs3 = genRefPoints(3)
-truePosRangeMes = [rangeMeasurement(truePos, r) for r in refs3]
-likelihoods3 = np.zeros([len(X), len(Y)])
-for i in range(len(X)):
-    for j in range(len(Y)):
-        likelihoods3[i, j] = MAPlikelihood(truePosRangeMes, [X[i][j], Y[i][j]], refs3)
+    # Generate Grid to make contours
+    delta = 0.005
+    x = np.arange(-2.0, 2.0, delta)
+    y = np.arange(-2.0, 2.0, delta)
+    X, Y = np.meshgrid(x, y)
 
-print(MAPlikelihood(truePosRangeMes, truePos, refs3))
-fig, ax = plt.subplots()
-CS = ax.contour(X, Y, likelihoods3, np.linspace(likelihoods3.min(), likelihoods3.max(), 30))
-ax.plot(truePos[0], truePos[1], 'r+', label='True Position')
-ax.plot([refs3[i][0] for i in range(len(refs3))], [refs3[i][1] for i in range(len(refs3))], 'bo', label='Reference point')
-ax.clabel(CS, inline=True, fontsize=10)
-ax.set_title('MAP Objective Function Contour Map: 3 Reference Points')
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-plt.legend()
-plt.show()
+    refs = genRefPoints(args.numRefs)
+    truePosRangeMes = [rangeMeasurement(truePos, r) for r in refs]
+    likelihoods = np.zeros([len(X), len(Y)])
+    for i in range(len(X)):
+        for j in range(len(Y)):
+            likelihoods[i, j] = MAPlikelihood(truePosRangeMes, [X[i][j], Y[i][j]], refs)
 
-# 4 ref points
-refs4 = genRefPoints(4)
-truePosRangeMes = [rangeMeasurement(truePos, r) for r in refs4]
-likelihoods4 = np.zeros([len(X), len(Y)])
-for i in range(len(X)):
-    for j in range(len(Y)):
-        likelihoods4[i, j] = MAPlikelihood(truePosRangeMes, [X[i][j], Y[i][j]], refs4)
-
-print(MAPlikelihood(truePosRangeMes, truePos, refs4))
-fig, ax = plt.subplots()
-CS = ax.contour(X, Y, likelihoods4, np.linspace(likelihoods4.min(), likelihoods4.max(), 30))
-ax.plot(truePos[0], truePos[1], 'r+', label='True Position')
-ax.plot([refs4[i][0] for i in range(len(refs4))], [refs4[i][1] for i in range(len(refs4))], 'bo', label='Reference point')
-ax.clabel(CS, inline=True, fontsize=10)
-ax.set_title('MAP Objective Function Contour Map: 4 Reference Points')
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-plt.legend()
-plt.show()
-
-# Plot Contours again, but using levels of 4 reference points
-fig, ax = plt.subplots()
-CS = ax.contour(X, Y, likelihoods1, np.linspace(likelihoods4.min(), likelihoods4.max(), 30))
-ax.plot(truePos[0], truePos[1], 'r+', label='True Position')
-ax.plot([refs1[i][0] for i in range(len(refs1))], [refs1[i][1] for i in range(len(refs1))], 'bo', label='Reference point')
-ax.clabel(CS, inline=True, fontsize=10)
-ax.set_title('MAP Objective Function Contour Map: 1 Reference Point')
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-plt.legend()
-plt.show()
-
-fig, ax = plt.subplots()
-CS = ax.contour(X, Y, likelihoods2, np.linspace(likelihoods4.min(), likelihoods4.max(), 30))
-ax.plot(truePos[0], truePos[1], 'r+', label='True Position')
-ax.plot([refs2[i][0] for i in range(len(refs2))], [refs2[i][1] for i in range(len(refs2))], 'bo', label='Reference point')
-ax.clabel(CS, inline=True, fontsize=10)
-ax.set_title('MAP Objective Function Contour Map: 2 Reference Points')
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-plt.legend()
-plt.show()
-
-fig, ax = plt.subplots()
-CS = ax.contour(X, Y, likelihoods3, np.linspace(likelihoods4.min(), likelihoods4.max(), 30))
-ax.plot(truePos[0], truePos[1], 'r+', label='True Position')
-ax.plot([refs3[i][0] for i in range(len(refs3))], [refs3[i][1] for i in range(len(refs3))], 'bo', label='Reference point')
-ax.clabel(CS, inline=True, fontsize=10)
-ax.set_title('MAP Objective Function Contour Map: 3 Reference Points')
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-plt.legend()
-plt.show()
-
-fig, ax = plt.subplots()
-CS = ax.contour(X, Y, likelihoods4, np.linspace(likelihoods4.min(), likelihoods4.max(), 30))
-ax.plot(truePos[0], truePos[1], 'r+', label='True Position')
-ax.plot([refs4[i][0] for i in range(len(refs4))], [refs4[i][1] for i in range(len(refs4))], 'bo', label='Reference point')
-ax.clabel(CS, inline=True, fontsize=10)
-ax.set_title('MAP Objective Function Contour Map: 4 Reference Points')
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-plt.legend()
-plt.show()
+    print(MAPlikelihood(truePosRangeMes, truePos, refs))
+    fig, ax = plt.subplots()
+    CS = ax.contour(X, Y, likelihoods, np.linspace(likelihoods.min(), likelihoods.max(), 30))
+    ax.plot(truePos[0], truePos[1], 'r+', label='True Position')
+    ax.plot([refs[i][0] for i in range(len(refs))], [refs[i][1] for i in range(len(refs))], 'bo',
+            label='Reference point')
+    ax.clabel(CS, inline=True, fontsize=10)
+    ax.set_title('MAP Objective Function Contour Map: ' + str(args.numRefs) + ' Reference Point(s)')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    plt.legend()
+    plt.show()
